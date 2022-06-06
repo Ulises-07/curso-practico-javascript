161e7f548171ae45a408f45548401550b104b20c
Ulises Roldan <leonel.matamoros21@gmail.com>
2022-06-05 21:55:38 -0600

    Concecte HTML con JavaScript, y cree los botones que me muestren resultados.

--- figuras.js
+++ figuras.js
@@ -1,57 +1,103 @@
 //Código del cuadrado.
 console.group("Cuadrado");
-    const lado_cuadrado = 5;
-    console.log("Los lados del cuadrado miden: " + lado_cuadrado +  "cm.");
+    // const lado_cuadrado = 5;
+    // console.log("Los lados del cuadrado miden: " + lado_cuadrado +  "cm.");
 
-    //Perimetro
-    const perimetro_cuadrado = lado_cuadrado * 4;
-    console.log("El perímetro del cuadrado es: " +  perimetro_cuadrado + "cm.");
+    //Perímetro
+    function perimetro_cuadrado (lado)
+    {
+        return lado * 4;
+    }
+    //console.log("El perímetro del cuadrado es: " +  perimetro_cuadrado + "cm.");
 
     //Area
-    const area_cuadrado = lado_cuadrado * lado_cuadrado;
-    console.log("El area del cuadrado es: " + area_cuadrado + "cm^2.");
+    function area_cuadrado (lado)
+    { 
+        return lado * lado;
+    }
+    //console.log("El area del cuadrado es: " + area_cuadrado + "cm^2.");
 console.groupEnd();
 
 
 //Código del triangulo
 console.group("Triangulo");
-    const lado_triangulo1 = 6;
-    const lado_triangulo2 = 6;
-    const base_triangulo = 4;
-    const altura_triangulo = 5.5;
-    console.log("Los lados del triangulo miden: " 
-        + lado_triangulo1 
-        + "cm, " 
-        + lado_triangulo2 
-        + "cm, " 
-        + base_triangulo 
-        + "cm."
-    );
-    console.log("La altura del triangulo es de: " + altura_triangulo + "cm.");
+    // const lado_triangulo1 = 6;
+    // const lado_triangulo2 = 6;
+    // const base_triangulo = 4;
+    // const altura_triangulo = 5.5;
+    // console.log("Los lados del triangulo miden: " 
+    //     + lado_triangulo1 
+    //     + "cm, " 
+    //     + lado_triangulo2 
+    //     + "cm, " 
+    //     + base_triangulo 
+    //     + "cm."
+    // );
+    // console.log("La altura del triangulo es de: " + altura_triangulo + "cm.");
 
-    //Perimetro
-    const perimetro_triangulo = lado_triangulo1 + lado_triangulo2 + base_triangulo;
-    console.log("El perímetro del triangulo es: " +  perimetro_triangulo + " cm.");
+    //Perímetro
+    function perimetro_triangulo (lado1, lado2, base)
+    {
+        return lado1 + lado2 + base;
+    }
+    //console.log("El perímetro del triangulo es: " +  perimetro_triangulo + " cm.");
 
     //Area
-    const area_triangulo = (base_triangulo * altura_triangulo) / 2;
-    console.log("El area del triangulo es de: " + area_triangulo + "cm^2.");
+    function area_triangulo (base, altura)
+    {
+        return (base * altura) /2
+    } 
+    //console.log("El area del triangulo es de: " + area_triangulo + "cm^2.");
 console.groupEnd();
 
 //Código del triangulo
 console.group("Circulo");
-    const radio_circulo = 4;
-    const diametro_circulo = radio_circulo * 2;
+    //const radio_circulo = 4;
+    function diametro_circulo (radio)
+    {
+        return radio * 2;
+    }
+
     const PI = Math.PI;
 
-    const perimetro_circulo = diametro_circulo * PI;
-    const area_ciculo = (radio_circulo * radio_circulo) * PI;
+    function perimetro_circulo (radio)
+    {
+        const diametro = diametro_circulo(radio);
+        return diametro * PI;
+    }
 
-    console.log("El radio del ciculo es: " +  radio_circulo + " cm.");
+    function area_ciculo(radio)
+    {
+        return (radio * radio) * PI;
+    }
+    //const perimetro_circulo = diametro_circulo * PI;
+    // const area_ciculo = (radio_circulo * radio_circulo) * PI;
 
-    console.log("El diametro del circulo es: " + diametro_circulo + " cm.");
+    // console.log("El radio del ciculo es: " +  radio_circulo + " cm.");
 
-    console.log("El perímetro del circulo es: " + perimetro_circulo + " cm.");
+    // console.log("El diametro del circulo es: " + diametro_circulo + " cm.");
 
-    console.log("El area del circulo es: " + area_ciculo + " cm^2.");
+    // console.log("El perímetro del circulo es: " + perimetro_circulo + " cm.");
+
+    // console.log("El area del circulo es: " + area_ciculo + " cm^2.");
 console.groupEnd();
+
+
+//Conectar con HTML
+function calcular_perimetro_cuadrado()
+{
+    const input = document.getElementById("input_cuadrado");
+    const valor = input.value;
+
+    const perimetro = perimetro_cuadrado(valor);
+    alert("El perimetro del cuadrado es: " + perimetro);
+}
+
+function calcular_area_cuadrado()
+{
+    const input = document.getElementById("input_cuadrado");
+    const valor = input.value;
+
+    const area = area_cuadrado(valor);
+    alert("El area del cuadrado es: " + area);
+}
